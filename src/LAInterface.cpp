1fb034be624a2b743d7ec8f8bfbdc933318c0a67
Eureka22 <xf1280@gmail.com>
2015-10-24 16:58:28 +0800

    test

--- src/LAInterface.cpp
+++ src/LAInterface.cpp
@@ -1675,7 +1675,7 @@ int64 LAInterface::getAlignmentNumber() {
 }
 
 void LOverlap::addtype() {
-    if ((abpos > aes + CHI_THRESHOLD) and (aepos > aee - CHI_THRESHOLD) and (bbpos < bes + CHI_THRESHOLD)) {
+    if ((abpos > aes + CHI_THRESHOLD) and (aepos > aee - CHI_THRESHOLD) and (bbpos < bes + CHI_THRESHOLD) and (bepos < bee - CHI_THRESHOLD)) {
         aln_type = FORWARD;
     }
 	/**
@@ -1683,7 +1683,7 @@ void LOverlap::addtype() {
 	 B:        ==========>
 	**/
 
-    else if (( abpos < aes + CHI_THRESHOLD) and (aepos < aee - CHI_THRESHOLD) and (bepos >  bee - CHI_THRESHOLD )) {
+    else if (( abpos < aes + CHI_THRESHOLD) and (aepos < aee - CHI_THRESHOLD) and (bepos >  bee - CHI_THRESHOLD ) and (bbpos > bes + CHI_THRESHOLD) ) {
          aln_type = BACKWARD;
     }
 	/**
