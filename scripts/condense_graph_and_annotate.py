fc54aa8e6e963cb0b05d8caa74378bf122f8fb1b
Govinda Kamath <govinda.kamath@gmail.com>
2016-02-29 20:54:58 -0800

    Condense graph without ground truth

--- scripts/condense_graph_and_annotate.py
+++ scripts/condense_graph_and_annotate.py
@@ -67,7 +67,7 @@ def merge_path(g,in_node,node,out_node):
     
 def input1(flname):
     g = nx.DiGraph()
-    with open (filename) as f:
+    with open (flname) as f:
         for lines in f:
             lines1=lines.split()
             #print lines1
@@ -80,7 +80,7 @@ def input1(flname):
             
 def input2(flname):
     g = nx.DiGraph()
-    with open (filename) as f:
+    with open (flname) as f:
         for lines in f:
             lines1=lines.split()
             #print lines1
