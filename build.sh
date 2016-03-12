3774e75d5c78cdcb64c024553e97c08156cfe2e5
fxia22 <xf1280@gmail.com>
2016-03-12 20:34:08 +0800

    [change] re-organize project

--- build.sh
+++ build.sh
@@ -7,9 +7,12 @@ make -j 8
 cd $pwd/DALIGNER
 make -j 8
 
-cd $pwd/src
+cd $pwd/DASCRUBBER
+make -j 8
+
+cd $pwd
 mkdir build
-cd $pwd/src/build
+cd $pwd/build
 cmake .. -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9
 make -j 8
 
