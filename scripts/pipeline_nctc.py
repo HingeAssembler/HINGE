ff5d5c11819914d4810ba65a1cfdcb4d2ac3f19a
Govinda Kamath <govinda.kamath@gmail.com>
2016-04-24 23:59:03 -0700

    used subprocess to exit if it errors

--- scripts/pipeline_nctc.py
+++ scripts/pipeline_nctc.py
@@ -2,78 +2,86 @@
 
 import sys
 import os
-
+import subprocess
 
 bact_name = "ecoli"
 
 if len(sys.argv) >= 2:
-	bact_name = sys.argv[1]
+	bact_id = sys.argv[1]
 
 st_point = 0
 if len(sys.argv) >= 3:
 	st_point = int(sys.argv[2])
 
+base_path='/data/pacbio_assembly/pb_data/NCTC/'+bact_id+"/"
+
+
+if len(sys.argv) >= 4:
+	base_path = sys.argv[3]
 
 
 
-dextract_command = 'dextract -o '
-h5list = []
+fasta_names = [x for x in os.listdir(base_path) if x.endswith('.fasta')]
+assert len(fasta_names)==1
 
-fasta_name = ''
+fasta_name = fasta_names[0]
+bact_name = fasta_name.split('.fasta')[0]
 
-for filename in os.listdir('.'):
-	if len(filename.split('.')) > 2 and filename.split('.')[-2] == 'bax' and filename.split('.')[-1] == 'h5':
-		h5list.append(filename)
-		dextract_command = dextract_command + ' ' + filename
-		if filename.split('.')[-3] == '1':
-			fasta_name = filename.split('.1.bax.h5')[0] + '.fasta'
+print bact_name
 
-
-if st_point <= 0:
-	print dextract_command
-	os.system(dextract_command)
 
 if st_point <= 1:
-	print "fasta2DB "+bact_name+' '+fasta_name
-	os.system("fasta2DB "+bact_name+' '+fasta_name)
+	fasta2DB_cmd = "fasta2DB "+bact_name+' '+base_path+fasta_name
+	print fasta2DB_cmd
+	subprocess.check_output(fasta2DB_cmd.split(),cwd=base_path)
 
 if st_point <= 2:
-	print "DBsplit -x500 -s100 "+bact_name
-	os.system("DBsplit -x500 -s100 "+bact_name)
+	DBsplit_cmd = "DBsplit -x500 -s100 "+bact_name
+	print DBsplit_cmd
+	subprocess.check_output(DBsplit_cmd.split(),cwd=base_path)
 
 if st_point <= 3:
-	print "HPCdaligner -dal4 -t16 -e.7 -l500 -s100 "+bact_name+" | zsh"
-	os.system("HPCdaligner -dal4 -t16 -e.7 -l500 -s100 "+bact_name+" | zsh")
+	daligner_cmd = "HPCdaligner -t5 "+bact_name
+    daligner_shell_cmd = "csh -v daligner_cmd.sh"
+    print daligner_cmd
+    p = subprocess.call(daligner_cmd.split(),stdout=open(base_path+'daligner_cmd.sh','w') , cwd=base_path)
+    print daligner_shell_cmd
+    p2 = subprocess.check_output(daligner_shell_cmd.split(), cwd=base_path)
 
 if st_point <= 4:
-	print "rm "+bact_name+".*."+bact_name+".*"
-	os.system("rm "+bact_name+".*."+bact_name+".*")
+	remove_cmd = "rm "+bact_name+".*."+bact_name+".*"
+	print remove_cmd
+	subprocess.check_output(remove_cmd.split(),cwd=base_path)
 
 if st_point <= 5:
-	print "LAmerge "+bact_name+".las "+bact_name+".*.las"
-	os.system("LAmerge "+bact_name+".las "+bact_name+".*.las")
+	LAmerge_cmd = "LAmerge "+bact_name+".las "+bact_name+".*.las"
+	print LAmerge_cmd
+	subprocess.check_output(LAmerge_cmd.split(),cwd=base_path)
 
 if st_point <= 6:
-	print "rm "+bact_name+".*.las"
-	os.system("rm "+bact_name+".*.las")
+	remove_cmd2 = "rm "+bact_name+".*.las"
+	subprocess.check_output(remove_cmd2.split(),cwd=base_path)
 
 if st_point <= 7:
-	os.system("mkdir log")
+	subprocess.check_output("mkdir log".split(),cwd=base_path)
 
 if st_point <= 8:
-	print "DASqv -c100 "+bact_name+" "+bact_name+".las"
-	os.system("DASqv -c100 "+bact_name+" "+bact_name+".las")
+	DASqv_cmd = "DASqv -c100 "+bact_name+" "+bact_name+".las"
+	subprocess.check_output(DASqv_cmd.split(),cwd=base_path)
 
 if st_point <= 9:
-	print "Reads_filter --db "+bact_name+" --las "+bact_name+".las -x "+bact_name+" --config ~/AwesomeAssembler/utils/nominal.ini"
-	os.system("Reads_filter --db "+bact_name+" --las "+bact_name+".las -x "+bact_name+" --config ~/AwesomeAssembler/utils/nominal.ini")
+	Reads_filter_cmd = "Reads_filter --db "+bact_name+" --las "+bact_name+".las -x "+bact_name+" --config ~/AwesomeAssembler/utils/nominal.ini"
+	print Reads_filter_cmd
+	subprocess.check_output(Reads_filter_cmd.split(),cwd=base_path)
 
 if st_point <= 10:
-	print "hinging --db "+bact_name+" --las "+bact_name+".las -x "+bact_name+" --config ~/AwesomeAssembler/utils/nominal.ini -o "+bact_name
-	os.system("hinging --db "+bact_name+" --las "+bact_name+".las -x "+bact_name+" --config ~/AwesomeAssembler/utils/nominal.ini -o "+bact_name)
+	hinging_cmd = "hinging --db "+bact_name+" --las "+bact_name+".las -x "+bact_name+" --config ~/AwesomeAssembler/utils/nominal.ini -o "+bact_name
+	print hinging_cmd
+	subprocess.check_output(hinging_cmd.split(), cwd=base_path)
 
 if st_point <= 11:
-	print "python ~/AwesomeAssembler/scripts/pruning_and_clipping.py "+bact_name+".edges.hinges "+bact_name+".hinge.list A"
-	os.system("python ~/AwesomeAssembler/scripts/pruning_and_clipping.py "+bact_name+".edges.hinges "+bact_name+".hinge.list A")
+	pruning_cmd = "python ~/AwesomeAssembler/scripts/pruning_and_clipping.py "+bact_name+".edges.hinges "+bact_name+".hinge.list A"
+	print pruning_cmd
+	subprocess.check_output(pruning_cmd.split(), cwd=base_path)
 
 
