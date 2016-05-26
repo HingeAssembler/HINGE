9b033d6765473375e82fcfaa109a37f4dcb6a8ef
Govinda Kamath <govinda.kamath@gmail.com>
2016-05-26 13:56:44 -0700

    fixed bug in i/o

--- scripts/pruning_and_clipping.py
+++ scripts/pruning_and_clipping.py
@@ -1116,10 +1116,10 @@ with open (flname) as f:
                 read_b_start_raw=rb_start_raw,read_b_end_raw=rb_end_raw)
             G.add_edge(lines1[1] + "_" + str(1-int(lines1[4])), lines1[0] + "_" + str(1-int(lines1[3])),
                 hinge_edge=int(lines1[5]),intersection=1,length=e1_match_len,z=0,
-                read_a_start=ra_start,read_a_end=ra_end,
-                read_b_start=rb_start,read_b_end=rb_end,
-                read_a_start_raw=ra_start_raw,read_a_end_raw=ra_end_raw,
-                read_b_start_raw=rb_start_raw,read_b_end_raw=rb_end_raw)
+                read_a_start=rb_start,read_a_end=rb_end,
+                read_b_start=ra_start,read_b_end=ra_end,
+                read_a_start_raw=rb_start_raw,read_a_end_raw=rb_end_raw,
+                read_b_start_raw=ra_start_raw,read_b_end_raw=ra_end_raw)
         else:
             G.add_edge(lines1[0] + "_" + lines1[3], lines1[1] + "_" + lines1[4],
                 hinge_edge=int(lines1[5]),intersection=0,length=e1_match_len,z=0,
@@ -1129,10 +1129,10 @@ with open (flname) as f:
                 read_b_start_raw=rb_start_raw,read_b_end_raw=rb_end_raw)
             G.add_edge(lines1[1] + "_" + str(1-int(lines1[4])), lines1[0] + "_" + str(1-int(lines1[3])),
                 hinge_edge=int(lines1[5]),intersection=0,length=e1_match_len,z=0,
-                read_a_start=ra_start,read_a_end=ra_end,
-                read_b_start=rb_start,read_b_end=rb_end,
-                read_a_start_raw=ra_start_raw,read_a_end_raw=ra_end_raw,
-                read_b_start_raw=rb_start_raw,read_b_end_raw=rb_end_raw)
+                read_a_start=rb_start,read_a_end=rb_end,
+                read_b_start=ra_start,read_b_end=ra_end,
+                read_a_start_raw=rb_start_raw,read_a_end_raw=rb_end_raw,
+                read_b_start_raw=ra_start_raw,read_b_end_raw=ra_end_raw)
 
 
         
