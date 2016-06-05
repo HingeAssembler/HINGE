f4359e2b465d6c820073eda6b44e9f6fc47d9f56
Ilan Shomorony <ilanshom@gmail.com>
2016-06-04 19:05:50 -0700

    updated n50 script

--- scripts/compute_n50_from_draft.py
+++ scripts/compute_n50_from_draft.py
@@ -1,5 +1,7 @@
 import sys
+import os
 import networkx as nx
+from Bio import SeqIO
 
 
 
@@ -25,30 +27,87 @@ def comp_n50(contig_vec):
     return 0.5*(min_n50+max_n50)
 
 
+hinging_n50 = -1
+hinging_comp_n50 = -1
+hgap_n50 = -1
+
+count = 0
+count1 = 0
+count2 = 0
 
 
-flname = sys.argv[1]
-g = nx.read_graphml(flname)
+fullpath = '/data/pacbio_assembly/pb_data/NCTC/'
+
+for nctc_name in os.listdir(fullpath):
+	if 'NCTC' not in nctc_name:
+		continue
+
+	mypath = fullpath+nctc_name
+
+	if not os.path.isdir(mypath):
+		continue
+
+	mypath = mypath+'/'
+
+	count += 1
+
+	draft_file = [x for x in os.listdir(mypath) if 'draft.graphml' in x]
+
+	try:
+
+		# flname = sys.argv[1]
+		g = nx.read_graphml(mypath+draft_file[0])
+
+		contig_lengths = []
+		component_lengths = []
 
 
-contig_lengths = []
-component_lengths = []
+		for u in g.nodes():
+		    contig_lengths.append(len(g.node[u]['segment']))
+		    
+		for c in nx.weakly_connected_components(g):
+		    # we use set() so that we cannot double-count a two reverse complementary contigs
+		    # in the same component
+		    component_lengths.append(sum(set([len(g.node[u]['segment']) for u in c])))
+		    
+		component_lengths = set(component_lengths)
+
+		hinging_n50 = comp_n50(contig_lengths)
+		hinging_comp_n50 = comp_n50(component_lengths)
+
+		count1+=1
+
+	except:
+		pass
+
+	# print "contig n50: "+str(comp_n50(contig_lengths))
+	# print "component n50: "+str(comp_n50(component_lengths))
 
 
-for u in g.nodes():
-    contig_lengths.append(len(g.node[u]['segment']))
-    
-for c in nx.weakly_connected_components(g):
-    # we use set() so that we cannot double-count a two reverse complementary contigs
-    # in the same component
-    component_lengths.append(sum(set([len(g.node[u]['segment']) for u in c])))
-    
-component_lengths = set(component_lengths)
+	hgap_file = [x for x in os.listdir(mypath) if 'hgap.fasta' in x]
 
 
+	try:
 
-print "contig n50: "+str(comp_n50(contig_lengths))
-print "component n50: "+str(comp_n50(component_lengths))
+		hgap_file = hgap_file[0]
+		hgap_contigs = [len(x) for x in SeqIO.parse(open(mypath+hgap_file),'fasta')]
 
+		hgap_n50 = comp_n50(hgap_contigs)
+
+		count2+=1
+
+	except:
+		pass
+
+
+	with open(mypath+nctc_name+'.n50','w') as f:
+		f.write('hinging'+'\t'+str(hinging_n50)+'\n')
+		f.write('hinging_comp'+'\t'+str(hinging_comp_n50)+'\n')
+		f.write('hgap'+'\t'+str(hgap_n50)+'\n')
+
+
+	print count
+	print count1
+	print count2
 
 
