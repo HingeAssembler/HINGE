ea2a931c708c493299384171df1021b6595db3cc
Govinda Kamath <govinda.kamath@gmail.com>
2016-07-15 23:01:08 -0700

    Update run.sh

--- demo/ecoli_demo/run.sh
+++ demo/ecoli_demo/run.sh
@@ -15,13 +15,13 @@ DASqv -c100 ecoli ecoli.las
 
 mkdir log
 
-Reads_filter --db ecoli --las ecoli.las -x ecoli --config ~/HINGE/utils/nominal.ini
-hinging --db ecoli --las ecoli.las -x ecoli --config ~/HINGE/utils/nominal.ini -o ecoli
+Reads_filter --db ecoli --las ecoli.las -x ecoli --config ../../utils/nominal.ini
+hinging --db ecoli --las ecoli.las -x ecoli --config ../../utils/nominal.ini -o ecoli
 
 pruning_and_clipping.py ecoli.edges.hinges ecoli.hinge.list demo
 
 get_draft_path.py $PWD ecoli ecolidemo.G2.graphml
-draft_assembly --db ecoli --las ecoli.las --prefix ecoli --config ~/HINGE/utils/nominal.ini --out ecoli.draft
+draft_assembly --db ecoli --las ecoli.las --prefix ecoli --config ../../utils/nominal.ini --out ecoli.draft
 
 correct_head.py ecoli.draft.fasta ecoli.draft.pb.fasta draft_map.txt 
 fasta2DB draft ecoli.draft.pb.fasta
@@ -29,6 +29,6 @@ HPCmapper draft ecoli | bash -v
 
 rm draft.*.ecoli.*.las
 LAmerge draft.ecoli.las draft.ecoli.*.las
-consensus draft ecoli draft.ecoli.las ecoli.consensus.fasta ~/HINGE/utils/nominal.ini
+consensus draft ecoli draft.ecoli.las ecoli.consensus.fasta ../../utils/nominal.ini
 
 get_consensus_gfa.py $PWD ecoli ecoldemo.G2.graphml ecoli.consensus.fasta
