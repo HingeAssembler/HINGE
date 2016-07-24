0bba9a38b7a0843588b1fd9194bbe4a08cd6c0e6
Eureka22 <xf1280@gmail.com>
2016-07-24 05:25:56 -0700

    add demo

--- demo/ecoli_demo/run.sh
+++ demo/ecoli_demo/run.sh
@@ -5,6 +5,7 @@ seqtk seq -a ecoli_p4_filtered.fastq > reads.fasta
 correct_head.py reads.fasta reads.pb.fasta map.txt
 fasta2DB ecoli reads.pb.fasta
 
+
 DBsplit ecoli
 
 HPCdaligner ecoli | bash -v
@@ -14,6 +15,8 @@ LAmerge ecoli.las ecoli.[0-9].las
 DASqv -c100 ecoli ecoli.las
 
 mkdir log
+
+
 
 Reads_filter --db ecoli --las ecoli.las -x ecoli --config ../../utils/nominal.ini
 hinging --db ecoli --las ecoli.las -x ecoli --config ../../utils/nominal.ini -o ecoli
@@ -31,4 +34,4 @@ rm draft.*.ecoli.*.las
 LAmerge draft.ecoli.las draft.ecoli.*.las
 consensus draft ecoli draft.ecoli.las ecoli.consensus.fasta ../../utils/nominal.ini
 
-get_consensus_gfa.py $PWD ecoli ecoldemo.G2.graphml ecoli.consensus.fasta
+get_consensus_gfa.py $PWD ecoli ecolidemo.G2.graphml ecoli.consensus.fasta
