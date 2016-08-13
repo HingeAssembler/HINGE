439649449da2460a80e45fd97d429ab27cea1701
ilanshom <ilanshom@gmail.com>
2016-08-12 19:00:37 -0700

    Update run.sh

--- demo/ecoli_demo/run.sh
+++ demo/ecoli_demo/run.sh
@@ -26,12 +26,3 @@ pruning_and_clipping.py ecoli.edges.hinges ecoli.hinge.list demo
 get_draft_path.py $PWD ecoli ecolidemo.G2.graphml
 draft_assembly --db ecoli --las ecoli.las --prefix ecoli --config ../../utils/nominal.ini --out ecoli.draft
 
-correct_head.py ecoli.draft.fasta ecoli.draft.pb.fasta draft_map.txt 
-fasta2DB draft ecoli.draft.pb.fasta
-HPC.daligner draft ecoli | bash -v 
-
-rm draft.*.ecoli.*.las
-LAmerge draft.ecoli.las draft.ecoli.*.las
-consensus draft ecoli draft.ecoli.las ecoli.consensus.fasta ../../utils/nominal.ini
-
-get_consensus_gfa.py $PWD ecoli ecolidemo.G2.graphml ecoli.consensus.fasta
