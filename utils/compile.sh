ef96a4abb8234d359781f315efcb85b5dfe1a579
fxia22 <xf1280@gmail.com>
2016-08-30 00:42:53 +0800

    add holy build box compile script

--- utils/compile.sh
+++ utils/compile.sh
@@ -5,24 +5,24 @@ source /hbb_exe/activate
 
 set -x
 
+yum install -y --quiet git wget ntp
+
+wget http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download --no-check-certificate
+tar -xvzf boost_1_55_0.tar.gz 
+cd boost_1_55_0/
+./bootstrap.sh --with-libraries=graph
+./b2 install
+
+export LDFLAGS="$LDFLAGS -pthread"
 
 pwd=/io
-cd $pwd/thirdparty/DAZZ_DB
-make -j 8
-
-cd $pwd/thirdparty//DALIGNER
-make -j 8
-
-cd $pwd/thirdparty/DASCRUBBER
-make -j 8
-
-cd $pwd/thirdparty/DEXTRACTOR
-make -j 8
 
 cd $pwd
+rm -rf build
 mkdir build
 cd $pwd/build
+
 cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
-make -j 8
+make
 
 exit $?
