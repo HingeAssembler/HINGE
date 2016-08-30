729bd50a786d3fe013950a408c27ba99b8c24f58
Eureka22 <xf1280@gmail.com>
2016-08-30 08:26:32 -0700

    add docker

--- utils/compile.sh
+++ utils/compile.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-set -e
-
-source /hbb_exe/activate
-
-set -x
-
-yum install -y --quiet git wget ntp
-
-wget http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download --no-check-certificate
-tar -xvzf boost_1_55_0.tar.gz 
-cd boost_1_55_0/
-./bootstrap.sh --with-libraries=graph
-./b2 install
-
-export LDFLAGS="$LDFLAGS -pthread"
 
 pwd=/io
 
@@ -22,7 +7,8 @@ rm -rf build
 mkdir build
 cd $pwd/build
 
-cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
+
+cmake .. -DCMAKE_C_COMPILER=gcc-4.8 -DCMAKE_CXX_COMPILER=g++-4.8
 make
 
 exit $?
