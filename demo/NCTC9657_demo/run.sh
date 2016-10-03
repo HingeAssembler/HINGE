6ce987dd20f4086808e6881f483c3e960ea685fb
Ilan Shomorony <ilanshom@gmail.com>
2016-10-03 16:33:59 -0700

    updated for new daligner

--- demo/NCTC9657_demo/run.sh
+++ demo/NCTC9657_demo/run.sh
@@ -11,7 +11,9 @@ DASqv -c100 NCTC9657 NCTC9657.las
 
 mkdir log
 
-Reads_filter --db NCTC9657 --las NCTC9657.las -x NCTC9657 --config ../../utils/nominal.ini
+
+
+Reads_filter --db NCTC9657 --las "NCTC9657.*.las" -x NCTC9657 --config ../../utils/nominal.ini
 hinging --db NCTC9657 --las NCTC9657.las -x NCTC9657 --config ../../utils/nominal.ini -o NCTC9657
 
 pruning_and_clipping.py NCTC9657.edges.hinges NCTC9657.hinge.list demo
@@ -19,12 +21,23 @@ pruning_and_clipping.py NCTC9657.edges.hinges NCTC9657.hinge.list demo
 get_draft_path.py $PWD NCTC9657 NCTC9657demo.G2.graphml
 draft_assembly --db NCTC9657 --las NCTC9657.las --prefix NCTC9657 --config ../../utils/nominal.ini --out NCTC9657.draft
 
+
+
+
+
+
 correct_head.py NCTC9657.draft.fasta NCTC9657.draft.pb.fasta draft_map.txt 
 fasta2DB draft NCTC9657.draft.pb.fasta
-HPC.daligner draft NCTC9657 | bash -v 
+HPC.daligner NCTC9657 draft | bash -v 
 
-rm draft.*.NCTC9657.*.las
-LAmerge draft.NCTC9657.las draft.NCTC9657.*.las
+# rm draft.*.NCTC9657.*.las
+# LAmerge draft.NCTC9657.las draft.NCTC9657.*.las
+
 consensus draft NCTC9657 draft.NCTC9657.las NCTC9657.consensus.fasta ../../utils/nominal.ini
 
 get_consensus_gfa.py $PWD NCTC9657 NCTC9657demo.G2.graphml NCTC9657.consensus.fasta
+
+
+
+
+
