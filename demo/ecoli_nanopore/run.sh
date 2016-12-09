bb9d3e675e459ee9bd6d515a112cb95ac6dc1211
Govinda Kamath <govinda.kamath@gmail.com>
2016-12-08 22:28:14 -0800

    added nanopore demo

---
+++ demo/ecoli_nanopore/run.sh
@@ -0,0 +1,41 @@
+wget http://s3.climb.ac.uk/nanopore/R9_Ecoli_K12_MG1655_lambda_MinKNOW_0.51.1.62.all.fasta
+#gunzip ecoli_p4_filtered.fastq.gz
+
+#seqtk seq -a ecoli_p4_filtered.fastq > reads.fasta
+hinge correct-head R9_Ecoli_K12_MG1655_lambda_MinKNOW_0.51.1.62.all.fasta reads.pb.fasta map.txt
+fasta2DB ecoli reads.pb.fasta
+
+
+DBsplit ecoli
+
+HPC.daligner ecoli | bash -v
+
+rm ecoli.*.ecoli.*.las
+LAmerge ecoli.las ecoli.[0-9].las
+DASqv -c100 ecoli ecoli.las
+
+mkdir log
+
+
+
+hinge filter --db ecoli --las ecoli --mlas -x ecoli --config ../../utils/nominal.ini
+hinge layout --db ecoli --las ecoli.las -x ecoli --config ../../utils/nominal.ini -o ecoli
+
+hinge clip-nanopore ecoli.edges.hinges ecoli.hinge.list demo
+
+hinge draft-path $PWD ecoli ecolidemo.G2.graphml
+hinge draft --db ecoli --las ecoli.las --prefix ecoli --config ../../utils/nominal.ini --out ecoli.draft
+
+
+
+hinge correct-head ecoli.draft.fasta ecoli.draft.pb.fasta draft_map.txt 
+fasta2DB draft ecoli.draft.pb.fasta
+
+HPC.daligner ecoli draft | bash -v 
+
+#rm draft.*.ecoli.*.las
+#LAmerge draft.ecoli.las draft.ecoli.*.las
+
+hinge consensus draft ecoli draft.ecoli.las ecoli.consensus.fasta ../../utils/nominal.ini
+
+hinge gfa $PWD ecoli  ecoli.consensus.fasta
